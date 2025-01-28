16f8d6457b89b8f39a4a30c792f6b83ae02aa37e
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2025-01-28 19:52:14 +0100

    Discones fisicas

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ ws_server.on('connection', function(conn){
 
 		player1.send( JSON.stringify(info) );
 		
+
+		player1.on('close', function(){
+			player1 = null;
+			if (player2 != null){
+				let data = {dis: 1};
+
+				player2.send(JSON.stringify(data));
+			}
+		});
+
 		player1.on('message', function (msg){	
 			
 			if (player2 == null)
@@ -45,21 +55,61 @@ ws_server.on('connection', function(conn){
 				player2.send(JSON.stringify(info));
 			}
 			else if (info.s1 != null){
+					
 				player2.send(JSON.stringify(info));
+
+				if (info.s1 >= 3 || info.s2 >= 3){
+					
+					let data;
+
+					if (info.s1 < info.s2){
+						data = {
+							game_over: true,
+							win: 2 
+						};	
+					}
+					else{
+						data = {
+							game_over: true,
+							win: 1 
+						};	
+						
+					}
+
+					
+					player1.send(JSON.stringify(data));
+					player2.send(JSON.stringify(data));
+				}
 			}
 		});
 
 
 	}
 	else if (player2 == null){
+		
 		player2 = conn;
 		let info = {
 			player_num: 2
 		};
 	
 		player2.send( JSON.stringify(info) );
+		setTimeout(function(){
+		let data = {game_start: true};
 
+		player1.send(JSON.stringify(data));
+		player2.send(JSON.stringify(data));
+		
+		}, 500);
 
+		player2.on('close', function(){
+			player2 = null;
+
+			if (player1 != null){
+				data = {dis: 2};
+
+				player1.send(JSON.stringify(data));
+			}
+		});
 		player2.on('message', function (msg){
 
 			if (player1 == null)
